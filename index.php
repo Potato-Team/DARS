3bad3e0a48886077f03d90b16f2680b91b8e48d8
Rafi Khan <rafathasankhan@gmail.com>
2018-11-18 00:39:12 +0600

    updated

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-$load = $_GET['load'];
-if($load == 'yes'){
+if(isset($_GET['logout'])) {
+  if($_GET['logout']=='yes'){
+    unset($_COOKIE['user']);
+  }
+}
+if(isset($_COOKIE['user'])){
+  echo 'Cookie Found<br>';
+  echo $_COOKIE['user'];
+  include('login.html');
+}else{
+  echo 'Cookie Not Found<br>';
 include('login.html');
 }
  ?>
--- login.php
+++ login.php
@@ -2,13 +2,14 @@
   $uname =  $_GET['uname'];
   $psw = $_GET['psw'];
   $rem = '';
-  try {
-     $rem = $_GET['remember'];
-  } catch (Exception $e) {
-    echo $e;
-          }
-  echo $uname." ".hash('ripemd160',$psw)." ".$rem;
-  if($rem == 'on'){
-    setcookie('user', $psw." ".hash('ripemd160', $psw),time() + (86400 * 30), "/");
+  if(isset($_GET['remember'])){
+    if($_GET['remember']=='on'){
+      setcookie('user', $psw." ".hash('ripemd160', $psw),time() + (86400 * 30), "/");
+    }
   }
+  echo $uname." ".hash('ripemd160',$psw)." ".$rem;
+
 ?>
+<form  action="index.php" method="GET">
+<button type="submit" name="logout" value="yes">Log out</button>
+</form>
