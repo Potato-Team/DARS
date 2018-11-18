bcfa74cd28805fb01074a8522032e112f7ef4579
Rafi Khan <rafathasankhan@gmail.com>
2018-11-18 07:29:19 +0600

    updated

--- index.php
+++ index.php
@@ -1,15 +1,73 @@
 <?php
-if(isset($_GET['logout'])) {
-  if($_GET['logout']=='yes'){
-    unset($_COOKIE['user']);
+  function clearCookies($clearSession = false){
+    $past = time() - 3600;
+    if ($clearSession === false)
+        $sessionId = session_id();
+    foreach ($_COOKIE as $key => $value)
+    {
+        if ($clearSession !== false || $value !== $sessionId)
+            setcookie($key, $value, $past, '/');
+    }
   }
-}
-if(isset($_COOKIE['user'])){
-  echo 'Cookie Found<br>';
-  echo $_COOKIE['user'];
-  include('login.html');
-}else{
-  echo 'Cookie Not Found<br>';
-include('login.html');
-}
+  if (false) {
+    require('login.php');
+    getCookie();
+  }else {
+    if(isset($_GET['uname'])&&isset($_GET['psw'])){
+      require('login.php');
+      $cookies=false;
+      $user = $_GET['uname'];
+      $pass = hash('ripemd160',$_GET['psw']);
+      if(isset($_GET['remember'])){
+        if($_GET['remember']=='on'){
+          $cookies=true;
+          }
+      }
+      loginAttempt($user, $pass, $cookies);
+    }elseif (isset($_GET['logout'])){
+      if($_GET['logout']=='true'){
+        echo "<style>
+        .alert {
+            margin: auto;
+            width: 400px;
+            padding: 20px;
+            background-color: #f44336;
+            color: white;
+            opacity: 1;
+            transition: opacity 0.6s;
+            margin-bottom: 15px;
+        }
+
+        .alert.info {background-color: #2196F3;}
+
+        .closebtn {
+            margin-left: 15px;
+            color: white;
+            font-weight: bold;
+            float: right;
+            font-size: 22px;
+            line-height: 20px;
+            cursor: pointer;
+            transition: 0.3s;
+        }
+
+        .closebtn:hover {
+            color: black;
+        }</style>
+        <body><div class=\"alert info\">
+              <span class=\"closebtn\">&times;</span>
+              <strong>Logged Out!</strong>
+              </div></body>";
+        clearCookies();
+        include('login.html');
+      }
+    }else{
+      include('login.html');
+    }
+    if (isset($_GET['attempt'])) {
+      if($_GET['attempt']=='false'){
+        echo "User Not Found";
+      }
+    }
+  }
  ?>
--- login.php
+++ login.php
@@ -1,15 +1,42 @@
+
 <?php
-  $uname =  $_GET['uname'];
-  $psw = $_GET['psw'];
-  $rem = '';
-  if(isset($_GET['remember'])){
-    if($_GET['remember']=='on'){
-      setcookie('user', $psw." ".hash('ripemd160', $psw),time() + (86400 * 30), "/");
+  function onSucess(){
+    echo "<form  action=\"index.php\" method=\"GET\">
+    <button type=\"submit\" name=\"logout\" value=\"true\">Log out</button>
+    </form>";
+    include('user.html');
+  }
+
+  function storeCookie($user,$pass){
+    setcookie('uname',$user,time() + (86400 * 30), "/");
+    setcookie('psw',$pass,time() + (86400 * 30), "/");
+  }
+
+  function getCookie(){
+    loginAttempt($_COOKIE['uname'],hash('ripemd160',$_COOKIE['psw']),false);
+  }
+
+  function loginAttempt($user, $pass, $cookies){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "dars";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $sql = "SELECT count(*) as 'found' FROM tbl_user WHERE cuname='$user' AND hpsw='$pass'";
+    $result = $conn -> query($sql) or die();
+    $data = array();
+    foreach ($result as $row) {
+      $data[] =  $row;
+    }
+    echo $result->fetch_assoc();
+    if($data[0]['found']==1){
+      onSucess();
+      if($cookies==true){
+        storeCookie($user,$pass);
+      }
+    }else{
+      header('Location: index.php?attempt=false');
     }
   }
-  echo $uname." ".hash('ripemd160',$psw)." ".$rem;
 
 ?>
-<form  action="index.php" method="GET">
-<button type="submit" name="logout" value="yes">Log out</button>
-</form>
