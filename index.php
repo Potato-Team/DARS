5be74351169715d405c7ab7d96523d36f460f81b
Rafi Khan <rafathasankhan@gmail.com>
2018-11-26 18:45:02 +0600

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,38 @@
     }
     if (isset($_GET['attempt'])) {
       if($_GET['attempt']=='false'){
-        echo "User Not Found";
+        echo "<style>
+        .alert {
+            margin: auto;
+            width: 400px;
+            padding: 20px;
+            background-color: #f44336;
+            color: white;
+            opacity: 1;
+            transition: opacity 0.6s;
+            margin-bottom: 15px;
+        }
+
+        .alert.info {background-color: #2196F3;}
+
+        .closebtn {
+            margin-left: 15px;
+            color: white;
+            font-weight: bold;
+            float: right;
+            font-size: 22px;
+            line-height: 20px;
+            cursor: pointer;
+            transition: 0.3s;
+        }
+
+        .closebtn:hover {
+            color: black;
+        }</style>
+        <body><div class=\"alert info\">
+              <span class=\"closebtn\">&times;</span>
+              <strong>User Not Found!</strong>
+              </div></body>";
       }
     }
   }
